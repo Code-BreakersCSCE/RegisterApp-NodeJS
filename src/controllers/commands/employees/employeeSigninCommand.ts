b5954c9a2faf6fb7a22708e92073690b75c6ba47
sam <swpost@uark.edu>
2021-02-23 19:47:51 -0600

    added function to verify password

--- src/controllers/commands/employees/employeeSigninCommand.ts
+++ src/controllers/commands/employees/employeeSigninCommand.ts
@@ -1,5 +1,6 @@
 import { EmployeeModel } from "../models/employeeModel";
 import* as employeeInfo from "../models/employeeModel";
+import { DatabaseConnection } from "../models/databaseConnection";
 
 
 interface signIn
@@ -35,4 +36,16 @@ function verifyCredentals(credentals:signIn)
 function findEmployee(id: signIn)
 {
     return employeeInfo.queryByEmployeeId(Number(id.employeeId));
+}
+
+function checkPassword(password: signIn, employee: EmployeeModel)
+{
+    if(password.password == String(employee.password))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
