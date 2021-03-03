fa789bc9a1357f7faf7908805ab5e62baf57bd5e
BigCMac <77807562+BigCMac@users.noreply.github.com>
2021-03-02 18:32:24 -0600

    initial addition of the validate form function
    added body and inputs to validate form

--- public/scripts/signIn.js
+++ public/scripts/signIn.js
@@ -2,7 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
 	// TODO: Anything you want to do when the page is loaded?
 });
 
-function validateForm() {
-	// TODO: Validate the user input
-	return true;
+function validateForm(empID, empPass) {
+	let numTest = /^\d+$/.test(empID);
+	
+	if (numTest == false) {
+		return false; //this return case is for invalid employee ID
+	} else if (empPass === "") {
+		return false; //this return case is for invalid employee password
+	} else {
+		return true;
+	}
 }
