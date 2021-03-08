2995a6720d9ef8811200f32105f73499450d7c37
sam <swpost@uark.edu>
2021-03-08 09:38:59 -0600

    made deleteActive return command response

--- src/controllers/commands/activeUsers/deleteActiceUser.ts
+++ src/controllers/commands/activeUsers/deleteActiceUser.ts
@@ -1,14 +1,33 @@
-import {createTransaction} from "../models/databaseConnection"
 import * as makeTransaction from "../models/databaseConnection"
 import { ActiveUserModel, queryBySessionKey } from "../models/activeUserModel";
- 
+import { CommandResponse, Employee } from "../../typeDefinitions";
+import { Resources, ResourceKey } from "../../../resourceLookup";
 
 async function deleteActive(sessionId:string)   
 {
-    let transaction = await makeTransaction.createTransaction();
-    let user= await queryBySessionKey(sessionId, transaction)
-    if(user)
-    {
-        user.destroy()
-    }
+   return makeTransaction.createTransaction().then(function(transaction)
+   {
+       return queryBySessionKey(sessionId,transaction).then(function(user)
+       {
+           if(user)
+           {
+               user.destroy()
+               transaction.commit()
+               return <CommandResponse<ActiveUserModel>>
+               {
+                   status: 200,
+                   message: "user deleted"
+               }
+           }
+           else
+           {
+               transaction.rollback()
+               return <CommandResponse<ActiveUserModel>>
+               {
+                   status: 500,
+                   message: Resources.getString(ResourceKey.USER_SESSION_NOT_FOUND)
+               }
+           }
+       })
+   })
 }
