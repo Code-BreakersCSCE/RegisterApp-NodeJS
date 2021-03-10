037b275195ba26f382c615ed76e638f9a11c15c2
sam <swpost@uark.edu>
2021-03-10 17:09:47 -0600

    fixed command response type and return

--- src/controllers/commands/employees/activeEmployeeExistsQuery.ts
+++ src/controllers/commands/employees/activeEmployeeExistsQuery.ts
@@ -1,26 +1,29 @@
 import { EmployeeModel } from "../models/employeeModel";
 import* as employeeInfo from "../models/employeeModel";
-import { CommandResponse, Employee } from "../../typeDefinitions";
+import { ActiveUser, CommandResponse, Employee } from "../../typeDefinitions";
 import { Resources, ResourceKey } from "../../../resourceLookup";
 
  //functianlity for finding active user
 
-export const execute = async (): Promise<CommandResponse<Employee>> =>{
+export const execute = async (): Promise<CommandResponse<EmployeeModel>> =>{
     return employeeInfo.queryActiveExists()
-    .then((foundEmployee: EmployeeModel|null): Promise<CommandResponse<Employee>>=>
+    .then((foundEmployee: EmployeeModel|null): Promise<CommandResponse<EmployeeModel>>=>
     {
-        if(!employeeInfo)
+        if(!foundEmployee)
         {
-            return Promise.reject(<CommandResponse<Employee>>
+            return Promise.reject(<CommandResponse<EmployeeModel>>
                 {
                     status: 404, 
                     message: Resources.getString(ResourceKey.EMPLOYEE_NOT_FOUND)
                 });
         }
 
-        return Promise.resolve(<CommandResponse<Employee>>
+        return Promise.resolve(<CommandResponse<EmployeeModel>>
             {
                 status: 200,
+                data: foundEmployee
+               
+                
                 
             });
         
