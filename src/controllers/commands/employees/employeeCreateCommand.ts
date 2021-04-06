9da2f013e14b0c939233be5a03d771e64ecd3a36
sam <swpost@uark.edu>
2021-04-05 19:07:49 -0500

    formated document to match js and typescriptformat

--- src/controllers/commands/employees/employeeCreateCommand.ts
+++ src/controllers/commands/employees/employeeCreateCommand.ts
@@ -1,7 +1,7 @@
-import { CommandResponse, Employee,EmployeeSaveRequest } from "../../typeDefinitions";
-import  {EmployeeModel }from "../models/employeeModel";
+import { CommandResponse, Employee, EmployeeSaveRequest } from "../../typeDefinitions";
+import { EmployeeModel } from "../models/employeeModel";
 import { Resources, ResourceKey } from "../../../resourceLookup";
-import { hashString, mapEmployeeData,  } from "./helpers/employeeHelper";
+import { hashString, mapEmployeeData, } from "./helpers/employeeHelper";
 import { isBlankString } from "../helpers/helper";
 import { EmployeeClassification } from "../models/constants/entityTypes";
 
@@ -9,50 +9,47 @@ import { EmployeeClassification } from "../models/constants/entityTypes";
 
 
 
-export function validateSaveRequest(req: EmployeeSaveRequest,initalEmployee:boolean=false):CommandResponse<Employee>{
-    let invalidReq=false;
-    let errMesage:String="";
-    if(isBlankString(req.firstName)|| isBlankString(req.lastName)){
-        invalidReq=true;
-        errMesage="one of the name fields are blank";
+export function validateSaveRequest(req: EmployeeSaveRequest, initalEmployee: boolean = false): CommandResponse<Employee> {
+    let invalidReq = false;
+    let errMesage: String = "";
+    if (isBlankString(req.firstName) || isBlankString(req.lastName)) {
+        invalidReq = true;
+        errMesage = "one of the name fields are blank";
     }
-    else if(isBlankString(req.password))
-    {
-        invalidReq=true;
-        errMesage=Resources.getString(ResourceKey.EMPLOYEE_PASSWORD_INVALID)
+    else if (isBlankString(req.password)) {
+        invalidReq = true;
+        errMesage = Resources.getString(ResourceKey.EMPLOYEE_PASSWORD_INVALID)
     }
-    else if(!initalEmployee &&((req.classification==null) || isNaN(req.classification) || !(req.classification in req )))
-    {
-        errMesage=Resources.getString(ResourceKey.EMPLOYEE_TYPE_INVALID);
-        invalidReq=true;
+    else if (!initalEmployee && ((req.classification == null) || isNaN(req.classification) || !(req.classification in req))) {
+        errMesage = Resources.getString(ResourceKey.EMPLOYEE_TYPE_INVALID);
+        invalidReq = true;
     }
 
-    if(!invalidReq)
-    {
-        return <CommandResponse<Employee>>{ status:200};
+    if (!invalidReq) {
+        return <CommandResponse<Employee>>{ status: 200 };
     }
 
     return <CommandResponse<Employee>>{
-        status:422,
-        message:errMesage
+        status: 422,
+        message: errMesage
     };
 
 }
 
-export async function newEmployee(req:EmployeeSaveRequest, initalEmployee:boolean=false) {
-    let validRequest= validateSaveRequest(req);
-    if(validRequest.status==200){
-        let newEmployee :EmployeeModel=<EmployeeModel>
-        {
-            firstName: req.firstName,
-            lastName: req.lastName,
-            password: Buffer.from(hashString(req.password)),
-            active:true,
-            managerId:req.managerId,
-            classification: (!initalEmployee? <EmployeeClassification>req.classification:
-                EmployeeClassification.GeneralManager)
-        };
-        return EmployeeModel.create(newEmployee).then(function(newEmployee):CommandResponse<Employee>{
+export async function newEmployee(req: EmployeeSaveRequest, initalEmployee: boolean = false) {
+    let validRequest = validateSaveRequest(req);
+    if (validRequest.status == 200) {
+        let newEmployee: EmployeeModel = <EmployeeModel>
+            {
+                firstName: req.firstName,
+                lastName: req.lastName,
+                password: Buffer.from(hashString(req.password)),
+                active: true,
+                managerId: req.managerId,
+                classification: (!initalEmployee ? <EmployeeClassification>req.classification :
+                    EmployeeClassification.GeneralManager)
+            };
+        return EmployeeModel.create(newEmployee).then(function (newEmployee): CommandResponse<Employee> {
             return <CommandResponse<Employee>>{
                 status: 201,
                 data: mapEmployeeData(newEmployee)
