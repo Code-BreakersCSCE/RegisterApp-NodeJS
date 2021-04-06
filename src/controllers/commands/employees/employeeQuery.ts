da3c545b877f81debe5c0132cb1985c5eed9b81a
sam <swpost@uark.edu>
2021-04-05 19:08:39 -0500

    formated documet to mathc js and typescript convetions

--- src/controllers/commands/employees/employeeQuery.ts
+++ src/controllers/commands/employees/employeeQuery.ts
@@ -6,29 +6,27 @@ import { mapEmployeeData } from "./helpers/employeeHelper";
 
 
 
-export async function  findById(id:string) :Promise<CommandResponse<Employee>> {
-   if(id=="")
-   {
-       return Promise.reject(<CommandResponse<Employee>>{
-           status: 422,
-           message: Resources.getString(ResourceKey.EMPLOYEE_EMPLOYEE_ID_INVALID)
-       })
-   }
+export async function findById(id: string): Promise<CommandResponse<Employee>> {
+    if (id == "") {
+        return Promise.reject(<CommandResponse<Employee>>{
+            status: 422,
+            message: Resources.getString(ResourceKey.EMPLOYEE_EMPLOYEE_ID_INVALID)
+        })
+    }
 
-   return EmployeeRepository.queryById(id).then(function(Employee){
-       if(Employee)
-       {
-           return Promise.resolve(<CommandResponse<Employee>>{
-               status: 200,
-               data: mapEmployeeData(Employee)
-           });
-       }
+    return EmployeeRepository.queryById(id).then(function (Employee) {
+        if (Employee) {
+            return Promise.resolve(<CommandResponse<Employee>>{
+                status: 200,
+                data: mapEmployeeData(Employee)
+            });
+        }
 
-       return Promise.reject(<CommandResponse<Employee>>{
-           status:404,
-           message: Resources.getString(ResourceKey.EMPLOYEE_NOT_FOUND)
-       });
-   })
-    
+        return Promise.reject(<CommandResponse<Employee>>{
+            status: 404,
+            message: Resources.getString(ResourceKey.EMPLOYEE_NOT_FOUND)
+        });
+    })
+
 };
 
