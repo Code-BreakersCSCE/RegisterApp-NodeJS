9ecd2c0cb72845bc943af2ea3bf0e23fe96a406e
BigCMac <77807562+BigCMac@users.noreply.github.com>
2021-04-09 16:00:24 -0500

    Update employeeUpdateCommand.ts

--- src/controllers/commands/employees/employeeUpdateCommand.ts
+++ src/controllers/commands/employees/employeeUpdateCommand.ts
@@ -1,122 +1,81 @@
-import Sequelize from "sequelize";
-import * as Helper from "../helpers/helper";
-import { EmployeeModel } from "../models/employeeModel";
-import * as EmployeeHelper from "./helpers/employeeHelper";
-import * as EmployeeRepository from "../models/employeeModel";
+import {
+	CommandResponse,
+	Employee,
+	EmployeeSaveRequest,
+} from "../../typeDefinitions";
+import * as employee from "../models/employeeModel";
 import { Resources, ResourceKey } from "../../../resourceLookup";
-import * as DatabaseConnection from "../models/databaseConnection";
+import { hashString, mapEmployeeData } from "./helpers/employeeHelper";
+import { isBlankString, isValidUUID } from "../helpers/helper";
 import { EmployeeClassification } from "../models/constants/entityTypes";
-import { CommandResponse, Employee, EmployeeSaveRequest } from "../../typeDefinitions";
-
-const buildUpdateObject = (employeeSaveRequest: EmployeeSaveRequest): Object => {
-	const updateObject: any = {};
-
-	if (employeeSaveRequest.active != null) {
-		updateObject.active = employeeSaveRequest.active;
+import { validateSaveRequest } from "../employees/employeeCreateCommand";
+import * as DatabaseConnection from "../models/databaseConnection";
+import Sequelize from "sequelize";
+function createUpdateObject(req: EmployeeSaveRequest) {
+	const update: any = {};
+	if (req.firstName != null) {
+		update.firstName = req.firstName;
 	}
-	if (employeeSaveRequest.lastName != null) {
-		updateObject.lastName = employeeSaveRequest.lastName;
+	if (req.lastName != null) {
+		update.lastName = req.lastName;
 	}
-	if (employeeSaveRequest.firstName != null) {
-		updateObject.firstName = employeeSaveRequest.firstName;
+	if (req.active != null) {
+		update.active = req.active;
 	}
-	if (!Helper.isBlankString(employeeSaveRequest.password)) {
-		updateObject.password = Buffer.from(EmployeeHelper.hashString(employeeSaveRequest.password));
+	if (!isBlankString(req.password)) {
+		update.passwrod = Buffer.from(hashString(req.password));
 	}
-	if (employeeSaveRequest.classification != null) {
-		updateObject.classification = <EmployeeClassification>employeeSaveRequest.classification;
+	if (req.classification != null) {
+		update.classification = <EmployeeClassification>req.classification;
 	}
-	if (!Helper.isBlankString(employeeSaveRequest.managerId)
-		&& Helper.isValidUUID(<string>employeeSaveRequest.managerId)) {
-
-		updateObject.managerId = employeeSaveRequest.managerId;
+	if (!isBlankString(req.managerId) && isValidUUID(<string>req.managerId)) {
+		update.managerId = req.managerId;
 	}
 
-	return updateObject;
-};
+	return update;
+}
 
-const validateSaveRequest = (employeeSaveRequest: EmployeeSaveRequest): CommandResponse<Employee> => {
-	let errorMessage: string = "";
+export async function updateEmployee(req: EmployeeSaveRequest) {
+	const validRequest = validateSaveRequest(req);
+	if (validRequest.status == 200) {
+		return DatabaseConnection.createTransaction().then(function (trans) {
+			return employee
+				.queryById(<string>req.id, trans)
+				.then(function (employee) {
+					if (employee == null) {
+						return Promise.reject(<CommandResponse<Employee>>{
+							status: 404,
+							message: Resources.getString(
+								ResourceKey.EMPLOYEES_UNABLE_TO_QUERY
+							),
+						});
+					}
 
-	if ((employeeSaveRequest.firstName != null)
-		&& (employeeSaveRequest.firstName.trim() === "")) {
-
-		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_FIRST_NAME_INVALID);
-	} else if ((employeeSaveRequest.lastName != null)
-		&& (employeeSaveRequest.lastName.trim() === "")) {
-
-		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_LAST_NAME_INVALID);
-	} else if ((employeeSaveRequest.password != null)
-		&& (employeeSaveRequest.password.trim() === "")) {
-
-		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_PASSWORD_INVALID);
-	} else if ((employeeSaveRequest.classification != null)
-		&& (isNaN(employeeSaveRequest.classification)
-		|| !(employeeSaveRequest.classification in EmployeeClassification))) {
-
-		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_TYPE_INVALID);
-	} else if ((employeeSaveRequest.managerId != null)
-		&& !Helper.isValidUUID(employeeSaveRequest.managerId)) {
-
-		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_MANAGER_ID_INVALID);
-	}
-
-	return ((errorMessage === "")
-		? <CommandResponse<Employee>>{ status: 200 }
-		: <CommandResponse<Employee>>{
-			status: 422,
-			message: errorMessage
-		});
-};
-
-export const execute = async (
-	employeeSaveRequest: EmployeeSaveRequest
-): Promise<CommandResponse<Employee>> => {
-
-	const validationResponse: CommandResponse<Employee> =
-		validateSaveRequest(employeeSaveRequest);
-	if (validationResponse.status !== 200) {
-		return Promise.reject(validationResponse);
-	}
-
-	let updateTransaction: Sequelize.Transaction;
-
-	return DatabaseConnection.createTransaction()
-		.then((createdTransaction: Sequelize.Transaction): Promise<EmployeeModel | null> => {
-			updateTransaction = createdTransaction;
-
-			return EmployeeRepository.queryById(
-				<string>employeeSaveRequest.id,
-				updateTransaction);
-		}).then((queriedEmployee: (EmployeeModel | null)): Promise<EmployeeModel> => {
-			if (queriedEmployee == null) {
-				return Promise.reject(<CommandResponse<Employee>>{
-					status: 404,
-					message: Resources.getString(ResourceKey.EMPLOYEE_NOT_FOUND)
+					return employee.update(createUpdateObject(req), <
+						Sequelize.InstanceUpdateOptions
+					>{ Transaction: trans });
+				})
+				.then(function (employee) {
+					trans.commit();
+					return <CommandResponse<Employee>>{
+						status: 200,
+						data: mapEmployeeData(employee),
+					};
+				})
+				.catch(function (err) {
+					if (trans != null) {
+						trans.rollback();
+					}
+					return Promise.reject(<CommandResponse<Employee>>{
+						status: err.status || 500,
+						message:
+							err.message ||
+							Resources.getString(
+								ResourceKey.EMPLOYEE_UNABLE_TO_SAVE
+							),
+					});
 				});
-			}
-
-			return queriedEmployee.update(
-				buildUpdateObject(employeeSaveRequest),
-				<Sequelize.InstanceUpdateOptions>{
-					transaction: updateTransaction
-				});
-		}).then((updatedEmployee: EmployeeModel): CommandResponse<Employee> => {
-			updateTransaction.commit();
-
-			return <CommandResponse<Employee>>{
-				status: 200,
-				data: EmployeeHelper.mapEmployeeData(updatedEmployee)
-			};
-		}).catch((error: any): Promise<CommandResponse<Employee>> => {
-			if (updateTransaction != null) {
-				updateTransaction.rollback();
-			}
-
-			return Promise.reject(<CommandResponse<Employee>>{
-				status: (error.status || 500),
-				message: (error.messsage
-					|| Resources.getString(ResourceKey.EMPLOYEE_UNABLE_TO_SAVE))
-			});
 		});
-};
+	}
+	return Promise.reject(validRequest);
+}
