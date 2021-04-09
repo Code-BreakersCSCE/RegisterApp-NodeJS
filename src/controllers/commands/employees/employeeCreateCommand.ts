6a107adce86da23df63766e16121c7a18df35016
BigCMac <mcdaniel.colten@gmail.com>
2021-04-08 19:45:51 -0500

    formatting changes

--- src/controllers/commands/employees/employeeCreateCommand.ts
+++ src/controllers/commands/employees/employeeCreateCommand.ts
@@ -1,61 +1,70 @@
-import { CommandResponse, Employee, EmployeeSaveRequest } from "../../typeDefinitions";
+import {
+	CommandResponse,
+	Employee,
+	EmployeeSaveRequest,
+} from "../../typeDefinitions";
 import { EmployeeModel } from "../models/employeeModel";
 import { Resources, ResourceKey } from "../../../resourceLookup";
-import { hashString, mapEmployeeData, } from "./helpers/employeeHelper";
+import { hashString, mapEmployeeData } from "./helpers/employeeHelper";
 import { isBlankString } from "../helpers/helper";
 import { EmployeeClassification } from "../models/constants/entityTypes";
 
+export function validateSaveRequest(
+	req: EmployeeSaveRequest,
+	initalEmployee: boolean = false
+): CommandResponse<Employee> {
+	let invalidReq = false;
+	let errMesage: String = "";
+	if (isBlankString(req.firstName) || isBlankString(req.lastName)) {
+		invalidReq = true;
+		errMesage = "one of the name fields are blank";
+	} else if (isBlankString(req.password)) {
+		invalidReq = true;
+		errMesage = Resources.getString(ResourceKey.EMPLOYEE_PASSWORD_INVALID);
+	} else if (
+		!initalEmployee &&
+		(req.classification == null ||
+			isNaN(req.classification) ||
+			!(req.classification in req))
+	) {
+		errMesage = Resources.getString(ResourceKey.EMPLOYEE_TYPE_INVALID);
+		invalidReq = true;
+	}
 
+	if (!invalidReq) {
+		return <CommandResponse<Employee>>{ status: 200 };
+	}
 
-
-
-export function validateSaveRequest(req: EmployeeSaveRequest, initalEmployee: boolean = false): CommandResponse<Employee> {
-    let invalidReq = false;
-    let errMesage: String = "";
-    if (isBlankString(req.firstName) || isBlankString(req.lastName)) {
-        invalidReq = true;
-        errMesage = "one of the name fields are blank";
-    }
-    else if (isBlankString(req.password)) {
-        invalidReq = true;
-        errMesage = Resources.getString(ResourceKey.EMPLOYEE_PASSWORD_INVALID)
-    }
-    else if (!initalEmployee && ((req.classification == null) || isNaN(req.classification) || !(req.classification in req))) {
-        errMesage = Resources.getString(ResourceKey.EMPLOYEE_TYPE_INVALID);
-        invalidReq = true;
-    }
-
-    if (!invalidReq) {
-        return <CommandResponse<Employee>>{ status: 200 };
-    }
-
-    return <CommandResponse<Employee>>{
-        status: 422,
-        message: errMesage
-    };
-
+	return <CommandResponse<Employee>>{
+		status: 422,
+		message: errMesage,
+	};
 }
 
-export async function newEmployee(req: EmployeeSaveRequest, initalEmployee: boolean = false) {
-    let validRequest = validateSaveRequest(req);
-    if (validRequest.status == 200) {
-        let newEmployee: EmployeeModel = <EmployeeModel>
-            {
-                firstName: req.firstName,
-                lastName: req.lastName,
-                password: Buffer.from(hashString(req.password)),
-                active: true,
-                managerId: req.managerId,
-                classification: (!initalEmployee ? <EmployeeClassification>req.classification :
-                    EmployeeClassification.GeneralManager)
-            };
-        return EmployeeModel.create(newEmployee).then(function (newEmployee): CommandResponse<Employee> {
-            return <CommandResponse<Employee>>{
-                status: 201,
-                data: mapEmployeeData(newEmployee)
-            };
-        });
-
-    }
-    return Promise.reject(validRequest);
+export async function newEmployee(
+	req: EmployeeSaveRequest,
+	initalEmployee: boolean = false
+) {
+	let validRequest = validateSaveRequest(req);
+	if (validRequest.status == 200) {
+		let newEmployee: EmployeeModel = <EmployeeModel>{
+			firstName: req.firstName,
+			lastName: req.lastName,
+			password: Buffer.from(hashString(req.password)),
+			active: true,
+			managerId: req.managerId,
+			classification: !initalEmployee
+				? <EmployeeClassification>req.classification
+				: EmployeeClassification.GeneralManager,
+		};
+		return EmployeeModel.create(newEmployee).then(function (
+			newEmployee
+		): CommandResponse<Employee> {
+			return <CommandResponse<Employee>>{
+				status: 201,
+				data: mapEmployeeData(newEmployee),
+			};
+		});
+	}
+	return Promise.reject(validRequest);
 }
--- src/controllers/commands/employees/employeeQuery.ts
+++ src/controllers/commands/employees/employeeQuery.ts
@@ -1,32 +1,29 @@
-
 import { CommandResponse, Employee } from "../../typeDefinitions";
 import * as EmployeeRepository from "../models/employeeModel";
 import { Resources, ResourceKey } from "../../../resourceLookup";
 import { mapEmployeeData } from "./helpers/employeeHelper";
 
-
-
 export async function findById(id: string): Promise<CommandResponse<Employee>> {
-    if (id == "") {
-        return Promise.reject(<CommandResponse<Employee>>{
-            status: 422,
-            message: Resources.getString(ResourceKey.EMPLOYEE_EMPLOYEE_ID_INVALID)
-        })
-    }
+	if (id == "") {
+		return Promise.reject(<CommandResponse<Employee>>{
+			status: 422,
+			message: Resources.getString(
+				ResourceKey.EMPLOYEE_EMPLOYEE_ID_INVALID
+			),
+		});
+	}
 
-    return EmployeeRepository.queryById(id).then(function (Employee) {
-        if (Employee) {
-            return Promise.resolve(<CommandResponse<Employee>>{
-                status: 200,
-                data: mapEmployeeData(Employee)
-            });
-        }
+	return EmployeeRepository.queryById(id).then(function (Employee) {
+		if (Employee) {
+			return Promise.resolve(<CommandResponse<Employee>>{
+				status: 200,
+				data: mapEmployeeData(Employee),
+			});
+		}
 
-        return Promise.reject(<CommandResponse<Employee>>{
-            status: 404,
-            message: Resources.getString(ResourceKey.EMPLOYEE_NOT_FOUND)
-        });
-    })
-
-};
-
+		return Promise.reject(<CommandResponse<Employee>>{
+			status: 404,
+			message: Resources.getString(ResourceKey.EMPLOYEE_NOT_FOUND),
+		});
+	});
+}
