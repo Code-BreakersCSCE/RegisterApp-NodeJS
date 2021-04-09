867f5a4b2a41d950fa6489fb48db427be366c90b
wyd4189606 <78048481+wyd4189606@users.noreply.github.com>
2021-04-09 15:24:30 -0500

    Update employeeCreateCommand.ts

--- src/controllers/commands/employees/employeeCreateCommand.ts
+++ src/controllers/commands/employees/employeeCreateCommand.ts
@@ -1,73 +1,71 @@
-import {
-	CommandResponse,
-	Employee,
-	EmployeeSaveRequest,
-} from "../../typeDefinitions";
+import * as Helper from "../helpers/helper";
 import { EmployeeModel } from "../models/employeeModel";
+import * as EmployeeHelper from "./helpers/employeeHelper";
 import { Resources, ResourceKey } from "../../../resourceLookup";
-import { hashString, mapEmployeeData } from "./helpers/employeeHelper";
-import { isBlankString } from "../helpers/helper";
 import { EmployeeClassification } from "../models/constants/entityTypes";
+import { CommandResponse, Employee, EmployeeSaveRequest } from "../../typeDefinitions";
 
-export function validateSaveRequest(
-	req: EmployeeSaveRequest,
-	initalEmployee: boolean = false
-): CommandResponse<Employee> {
-	let invalidReq = false;
-	let errMesage: String = "";
-	if (isBlankString(req.firstName)) {
-		invalidReq = true;
-		errMesage = "first name is blank";
-	} else if (isBlankString(req.lastName)) {
-		invalidReq = true;
-		errMesage = "last name is blank";
-	} else if (isBlankString(req.password)) {
-		invalidReq = true;
-		errMesage = Resources.getString(ResourceKey.EMPLOYEE_PASSWORD_INVALID);
-	} else if (
-		!initalEmployee &&
-		(req.classification == null ||
-			isNaN(req.classification) ||
-			!(req.classification in req))
-	) {
-		errMesage = Resources.getString(ResourceKey.EMPLOYEE_TYPE_INVALID);
-		invalidReq = true;
+const validateSaveRequest = (
+	employeeSaveRequest: EmployeeSaveRequest,
+	isInitialEmployee: boolean = false
+): CommandResponse<Employee> => {
+
+	let errorMessage: string = "";
+
+	if (Helper.isBlankString(employeeSaveRequest.firstName)) {
+		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_FIRST_NAME_INVALID);
+	} else if (Helper.isBlankString(employeeSaveRequest.lastName)) {
+		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_LAST_NAME_INVALID);
+	} else if (Helper.isBlankString(employeeSaveRequest.password)) {
+		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_PASSWORD_INVALID);
+	} else if (!isInitialEmployee
+		&& ((employeeSaveRequest.classification == null)
+			|| isNaN(employeeSaveRequest.classification)
+			|| !(employeeSaveRequest.classification in EmployeeClassification))) {
+
+		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_TYPE_INVALID);
+	} else if (!Helper.isBlankString(employeeSaveRequest.managerId)
+		&& !Helper.isValidUUID(<string>employeeSaveRequest.managerId)) {
+
+		errorMessage = Resources.getString(ResourceKey.EMPLOYEE_MANAGER_ID_INVALID);
 	}
 
-	if (!invalidReq) {
-		return <CommandResponse<Employee>>{ status: 200 };
+	return ((errorMessage === "")
+		? <CommandResponse<Employee>>{ status: 200 }
+		: <CommandResponse<Employee>>{
+			status: 422,
+			message: errorMessage
+		});
+};
+
+export const execute = async (
+	employeeSaveRequest: EmployeeSaveRequest,
+	isInitialEmployee: boolean = false
+): Promise<CommandResponse<Employee>> => {
+
+	const validationResponse: CommandResponse<Employee> =
+		validateSaveRequest(employeeSaveRequest, isInitialEmployee);
+	if (validationResponse.status !== 200) {
+		return Promise.reject(validationResponse);
 	}
 
-	return <CommandResponse<Employee>>{
-		status: 422,
-		message: errMesage,
+	const employeeToCreate: EmployeeModel = <EmployeeModel>{
+		active: true,
+		lastName: employeeSaveRequest.lastName,
+		firstName: employeeSaveRequest.firstName,
+		managerId: employeeSaveRequest.managerId,
+		classification: (!isInitialEmployee
+			? <EmployeeClassification>employeeSaveRequest.classification
+			: EmployeeClassification.GeneralManager),
+		password: Buffer.from(
+			EmployeeHelper.hashString(employeeSaveRequest.password))
 	};
-}
 
-export async function newEmployee(
-	req: EmployeeSaveRequest,
-	initalEmployee: boolean = false
-) {
-	const validRequest = validateSaveRequest(req);
-	if (validRequest.status == 200) {
-		const newEmployee: EmployeeModel = <EmployeeModel>{
-			firstName: req.firstName,
-			lastName: req.lastName,
-			password: Buffer.from(hashString(req.password)),
-			active: true,
-			managerId: req.managerId,
-			classification: !initalEmployee
-				? <EmployeeClassification>req.classification
-				: EmployeeClassification.GeneralManager,
-		};
-		return EmployeeModel.create(newEmployee).then(function (
-			newEmployee
-		): CommandResponse<Employee> {
+	return EmployeeModel.create(employeeToCreate)
+		.then((createdEmployee: EmployeeModel): CommandResponse<Employee> => {
 			return <CommandResponse<Employee>>{
 				status: 201,
-				data: mapEmployeeData(newEmployee),
+				data: EmployeeHelper.mapEmployeeData(createdEmployee)
 			};
 		});
-	}
-	return Promise.reject(validRequest);
-}
+};
