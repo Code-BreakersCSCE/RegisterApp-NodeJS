79cd775199cdc9ac49bf18e3e3d54fcb14997a67
wyd4189606 <78048481+wyd4189606@users.noreply.github.com>
2021-04-09 15:38:26 -0500

    Update employeeDetailRoutes.ts

--- src/routes/employeeDetailRoutes.ts
+++ src/routes/employeeDetailRoutes.ts
@@ -1,29 +1,23 @@
-// employeeDetailRoutes.ts
-
 import express from "express";
 import { RouteLookup } from "../controllers/lookups/routingLookup";
 import * as EmployeeDetailRouteController from "../controllers/employeeDetailRouteController";
 
-function EmployeeDetailRoutes(server: express.Express) {
-	server.get(RouteLookup.EmployeeDetail, EmployeeDetailRouteController.start);
-
+function employeeDetailRoutes(server: express.Express) {
+	server.get(
+		RouteLookup.EmployeeDetail,
+		EmployeeDetailRouteController.start);
 	server.get(
 		(RouteLookup.EmployeeDetail + RouteLookup.EmployeeIdParameter),
 		EmployeeDetailRouteController.startWithEmployee);
 
 	server.patch(
-		RouteLookup.API +
-			RouteLookup.EmployeeDetail +
-			RouteLookup.EmployeeIdParameter,
-		EmployeeDetailRouteController.updateEmployee
-	);
+		(RouteLookup.API + RouteLookup.EmployeeDetail
+			+ RouteLookup.EmployeeIdParameter),
+		EmployeeDetailRouteController.updateEmployee);
 
 	server.post(
-		RouteLookup.API + RouteLookup.EmployeeDetail,
-		EmployeeDetailRouteController.createEmployee
-	);
-
-	// Add employee query
+		(RouteLookup.API + RouteLookup.EmployeeDetail),
+		EmployeeDetailRouteController.createEmployee);
 }
 
-module.exports.routes = EmployeeDetailRoutes;
+module.exports.routes = employeeDetailRoutes;
